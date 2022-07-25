0064a0024dee30bdc845502e9847df5d0b2ea2fa
abinandn1 <77420355+abinandn1@users.noreply.github.com>
2022-07-24 21:32:46 -0600

    colour updates

--- Frontend/src/pages/Homepage.js
+++ Frontend/src/pages/Homepage.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import image from '../pictures/chef-restaurant-logo-publicdomainvectors.svg';
 import {Button} from 'react-bootstrap';
+import "./Homepage.css";
+
 const Homepage = () => {
   return (
     
-    <div className='text-center'>   
-        <div className="display-3"> Cubed restaurant services</div>
-        <div className="display-6"> Optimizing restaurant businesses</div>
-        <Button href="./menu"> Order now</Button>
+    <div className='text-center'>
+      <div className='homepage'>
+        <div className="display-5"> Cubed Restaurant Services</div>
+        <div className="display-6"> Optimizing Restaurant Businesses </div>
+        <Button variant='light' href="./menu"> Order now</Button>
+        </div>
     </div>
     //placeholder for qr scanner
     
