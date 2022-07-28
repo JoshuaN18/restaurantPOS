d79d2973249aef6f7ad1bb92ae4ea5fdc7eb8133
Xuanhao Wang <67352673+ccBrita@users.noreply.github.com>
2022-07-27 21:16:32 -0600

    restaurant info

---
+++ Frontend/src/components/restaurantInfo.js
@@ -0,0 +1,50 @@
+import React from 'react'
+
+const restaurantInfo = (props) => {
+    const { name, image, price, reviews, rating, categories } =
+    props.route.params;
+    const formattedCategories = categories.map((cat) => cat.title).join(" • ");
+
+    const description = `${formattedCategories} ${
+        price ? " • " + price : ""
+    } • 🎫 • ${rating} ⭐ (${reviews}+)`;
+    return (
+    <div>
+      <RestaurantImage image={image} />
+      <RestaurantName name={name} />
+      <RestaurantDescription description={description} />
+    </div>
+  )
+}
+
+const RestaurantImage = (props) => (
+    <Image source={{ uri: props.image }} style={{ width: "100%", height: 180 }} />
+  );
+  
+  const RestaurantName = (props) => (
+    <Text
+      style={{
+        fontSize: 29,
+        fontWeight: "600",
+        marginTop: 10,
+        marginHorizontal: 15,
+      }}
+    >
+      {props.name}
+    </Text>
+  );
+  
+  const RestaurantDescription = (props) => (
+    <Text
+      style={{
+        marginTop: 10,
+        marginHorizontal: 15,
+        fontWeight: "400",
+        fontSize: 15.5,
+      }}
+    >
+      {props.description}
+    </Text>
+  );
+
+export default restaurantInfo
